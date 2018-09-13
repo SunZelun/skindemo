b0d34ab2915ba03d05c758716f0aadeff85c11d8
Sun <sun.z@pg.com>
2018-09-13 16:04:24 +0800

    init commit

---
+++ datasource.js
@@ -0,0 +1,41 @@
+var data = [
+    {
+        "consumerId": "1",
+        "consumerName": "Consumer 1",
+        "scores": {
+            "yandai": 67,
+            "yanjiao": 55,
+            "etou": 50,
+            "zuijiao": 83,
+            "lianjia": 71
+        },
+        "skinAge": 23,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    }
+];
