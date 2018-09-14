8ea9ed7de39860ca4542a9c599aea26a58b3a39e
Sun <sun.z@pg.com>
2018-09-14 10:39:25 +0800

    add selfies

--- datasource.js
+++ datasource.js
@@ -1,13 +1,202 @@
 var data = [
     {
-        "consumerId": "1",
-        "consumerName": "Consumer 1",
+        "consumerName": "张晓蓉",
         "scores": {
             "yandai": 67,
             "yanjiao": 55,
             "etou": 50,
             "zuijiao": 83,
             "lianjia": 71
+        },
+        "skinAge": 23,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    },
+    {
+        "consumerName": "吴明静",
+        "scores": {
+            "yandai": 30,
+            "yanjiao": 72,
+            "etou": 0,
+            "zuijiao": 52,
+            "lianjia": 33
+        },
+        "skinAge": 31,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    },
+    {
+        "consumerName": "茅玮",
+        "scores": {
+            "yandai": 56,
+            "yanjiao": 73,
+            "etou": 0,
+            "zuijiao": 25,
+            "lianjia": 55
+        },
+        "skinAge": 24,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    },
+    {
+        "consumerName": "方桐",
+        "scores": {
+            "yandai": 50,
+            "yanjiao": 57,
+            "etou": 0,
+            "zuijiao": 54,
+            "lianjia": 61
+        },
+        "skinAge": 28,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    },
+    {
+        "consumerName": "卜佳",
+        "scores": {
+            "yandai": 72,
+            "yanjiao": 85,
+            "etou": 0,
+            "zuijiao": 55,
+            "lianjia": 71
+        },
+        "skinAge": 26,
+        "details": [
+            {
+                "title": "您的嘴角肌肤还不错",
+                "icon": "",
+                "statement": "微笑,说话等频繁的动作易造成嘴角肌肤问题，当肌肤弹性下降时，形成嘴角细纹和法令纹。"
+            },
+            {
+                "title": "您的眼底肌肤还不错",
+                "icon": "",
+                "statement": "眼底的肌肤非常薄，使眼部的血管看起来更明显。"
+            },
+            {
+                "title": "您的眼角肌肤还不错",
+                "icon": "",
+                "statement": "眼部肌肤是最娇嫩的肌肤"
+            },
+            {
+                "title": "额头是您最急需改善的部位",
+                "icon": "",
+                "statement": "做皱眉，抬眉等表情时，在强烈的肌肉拉动下，额头肌肤很容易形成皱纹。"
+            },
+            {
+                "title": "脸颊是您的最佳部位",
+                "icon": "",
+                "statement": "随着年龄的增长，脸颊肌肤容易干瘪松弛"
+            }
+        ]
+    },
+    {
+        "consumerName": "吴佳雯",
+        "scores": {
+            "yandai": 49,
+            "yanjiao": 68,
+            "etou": 0,
+            "zuijiao": 33,
+            "lianjia": 53
         },
         "skinAge": 23,
         "details": [
